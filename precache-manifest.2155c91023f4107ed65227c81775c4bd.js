e2910200d69a83a5cfec1b0896f8fc10fe9c6c12
김주혜 <62602963+empty3331@users.noreply.github.com>
2021-06-21 15:47:53 +0900

    Add files via upload

---
+++ precache-manifest.2155c91023f4107ed65227c81775c4bd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "710fcfc7756d432e398c",
+    "url": "/css/app.18691dcb.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "6d7c3c5c5bdf19172d077b7b98eaf1cf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "710fcfc7756d432e398c",
+    "url": "/js/app.29ada75c.js"
+  },
+  {
+    "revision": "ae03a24991b10840dc31",
+    "url": "/js/chunk-vendors.986628f2.js"
+  },
+  {
+    "revision": "6a1ad00dd7d473f746f3897d0ce8c52e",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
